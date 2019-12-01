3f0f124f1cb734b02f6a6a584e81e7149d64abcf
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-01 11:24:31 +0000

    Setting Up SQLite

---
+++ DataAccess.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using DataAccess.Droid.Persistence;
+using DataAccess.Persistence;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace DataAccess.Droid.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ DataAccess.iOS/Persistence/SQLiteDb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using DataAccess.iOS;
+using DataAccess.Persistence;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace DataAccess.iOS
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
