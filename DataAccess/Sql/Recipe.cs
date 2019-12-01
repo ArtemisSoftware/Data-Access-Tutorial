cf7e70e76061c2d6b8bb34dccbaf38589d939521
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-01 12:00:06 +0000

    INotifyPropertyChanged Interface

--- DataAccess/Sql/Recipe.cs
+++ DataAccess/Sql/Recipe.cs
@@ -1,16 +1,44 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DataAccess.Sql
 {
-    class Recipe
+    public class Recipe : INotifyPropertyChanged
     {
+        private string _name;
+
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
         [MaxLength(255)]
-        public string Name { get; set; }
+        public string Name { 
+            get { return _name; }
+            set {
+
+                if(_name == value)
+                {
+                    return;
+                }
+                else
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            } 
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
